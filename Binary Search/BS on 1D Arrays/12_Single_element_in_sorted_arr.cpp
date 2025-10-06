32d06bf16bcd39188e278e299531bd123666526f
rahul-singh92 <rahulsinghjadoun09@gmail.com>
2025-10-06 22:17:25 +0530

    DSA Problem

---
+++ Binary Search/BS on 1D Arrays/12_Single_element_in_sorted_arr.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Brute. O(n)
+// int sin_ele(vector<int> vec, int n)
+// {
+//     int xorr = 0;
+//     for(int i = 0;i<n;i++)
+//     {
+//         xorr ^= vec[i];
+//     }
+//     return xorr;
+// }
+
+int sin_ele(vector<int> vec, int n)
+{
+    int xorr = 0;
+    for(int i = 0;i<n;i++)
+    {
+        xorr ^= vec[i];
+    }
+    return xorr;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> vec(n,0);
+    for(int i = 0;i<n;i++)
+    {
+        cin>>vec[i];
+    }
+    int ans = sin_ele(vec, n);
+    cout<<ans<<endl;
+}

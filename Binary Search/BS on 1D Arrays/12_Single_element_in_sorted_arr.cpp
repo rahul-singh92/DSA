60f055c10aeedefad3f0c70aabe3d3f4ef866208
rahul-singh92 <rahulsinghjadoun09@gmail.com>
2025-10-09 21:06:22 +0530

    DSA Problem

--- Binary Search/BS on 1D Arrays/12_Single_element_in_sorted_arr.cpp
+++ Binary Search/BS on 1D Arrays/12_Single_element_in_sorted_arr.cpp
@@ -15,12 +15,22 @@ using namespace std;
 
 int sin_ele(vector<int> vec, int n)
 {
-    int xorr = 0;
-    for(int i = 0;i<n;i++)
+    if(n == 1) return vec[0];
+    if(vec[0] != vec[1]) return vec[0];
+    if(vec[n-1] != vec[n-2]) return vec[n-1];
+
+    int low = 1, high = n-2;
+    while(low <= high)
     {
-        xorr ^= vec[i];
+        int mid = low + (high - low) / 2;  //Overflow Condition
+        if(vec[mid] != vec[mid + 1] && vec[mid] != vec[mid - 1])
+        {
+            return vec[mid];
+        }
+        if((mid % 2 == 1 && vec[mid - 1] == vec[mid]) || (mid %2 == 0 && vec[mid] == vec[mid + 1])) low = mid + 1;
+        else high = mid - 1;
     }
-    return xorr;
+    return -1;
 }
 
 int main()

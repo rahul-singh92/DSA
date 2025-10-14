bcab3222010d5066c4223e090d9f30046ec24c76
rahul-singh92 <rahulsinghjadoun09@gmail.com>
2025-10-14 21:28:09 +0530

    DSA Problem

--- Binary Search/BS on Answers/1_square_root_number_in_log_n.cpp
+++ Binary Search/BS on Answers/1_square_root_number_in_log_n.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <math.h>
 using namespace std;
 
---
+++ Binary Search/BS on Answers/2_Nth_Root_of_no_Using_BS.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+//Brute : Becasue we take whole linear search and also for func(log n) so O(m * log n)
+// long long func(int i, int n)
+// {
+//     long long ans = 1;
+//     long long base = i;
+//     while(n > 0)
+//     {
+//         if(n % 2)
+//         {
+//             n--;
+//             ans = ans * base;
+//         }
+//         else
+//         {
+//             n /= 2;
+//             base = base * base;
+//         }
+//     }
+//     return ans;
+// }
+// int nthRoot(int n, int m)
+// {
+//     for(int i = 1;i <= m; i++)
+//     {
+//         long long val = func(i, n);
+//         if(val == m * 1ll) return i;
+//         else if(val > m * 1ll) break;
+//     }
+//     return -1;
+// }
+
+//Better using BS: O(log2m * log2n) 
+int func(int mid, int n, int m)
+{
+    long long ans = 1;
+    for(int i = 1;i<=n;i++)
+    {
+        ans = ans * mid;
+        if(ans > m) return 2;
+    }
+    if(ans == m) return 1;
+    return 0;
+}
+int nthRoot(int n, int m)
+{
+    int low = 1, high = m;
+    while(low <= high)
+    {
+        int mid = low + (high - low ) / 2;
+        int midN = func(mid, n, m);
+        if(midN == 1) return mid;
+        else if(midN == 0) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int ans = nthRoot(n,m);
+    cout<<ans<<endl;
+}

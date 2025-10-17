f471925fe3cb847acb69ef6c874e36fb8b6f51d0
rahul-singh92 <rahulsinghjadoun09@gmail.com>
2025-10-17 17:09:27 +0530

    DSA Problem

---
+++ Binary Search/BS on Answers/8_Aggressive_cows.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//O(n*(max-min))
+// int agg_cow(vector<int> vec, int n, int k)
+// {
+//     sort(vec.begin(), vec.end());
+//     int maxi = vec[n-1], mini = vec[0];
+//     for(int i = 1; i <= maxi - mini; i++)
+//     {
+//         int count_cows = 1;
+//         int cow_coordinate = vec[0];
+//         for(int j = 1; j<n;j++)
+//         {
+//             if(vec[j] - cow_coordinate >= i)
+//             {
+//                 count_cows++;
+//                 cow_coordinate = vec[j];
+//             }
+//         }
+//         if(count_cows >= k) continue;
+//         else return i - 1;
+//     }
+// }
+
+int agg_cow(vector<int> vec, int n, int k)
+{
+    sort(vec.begin(), vec.end());
+    int maxi = vec[n-1], mini = vec[0];
+    int low = 1, high = maxi - mini;
+    while(low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        int cow_count = 1;
+        int cow_coordinate = vec[0];
+        for(int j = 1; j<n;j++)
+        {
+            if(vec[j] - cow_coordinate >= mid)
+            {
+                cow_count++;
+                cow_coordinate = vec[j];
+            }
+        }
+        if(cow_count >= k) low = mid + 1;
+        else high = mid - 1;
+    }
+    return high;
+}
+
+int main()
+{
+    int n, k;
+    cin>>n>>k;
+    vector<int> vec(n, 0);
+    for(int i = 0;i<n;i++)
+    {
+        cin>>vec[i];
+    }
+    int ans = agg_cow(vec, n, k);
+    cout<<ans<<endl;
+}

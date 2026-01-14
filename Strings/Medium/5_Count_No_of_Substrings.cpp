d82a51a24c8fbdc4cb0dfe319cb85b0c0de9099d
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-01-14 19:55:05 +0530

    DSA Practice

--- Strings/Medium/5_Count_No_of_Substrings.cpp
+++ Strings/Medium/5_Count_No_of_Substrings.cpp
@@ -2,21 +2,40 @@
 #include <string>
 using namespace std;
 
-int op_ca(string s)
+int op_ca(string s, int n)
 {
-    for(int i = 0; i < s.size(); i++)
+    int left = 0, res = 0;
+    unordered_map<char, int> freq;
+
+    for(int right = 0; right < s.size(); right++)
     {
-        for(int j = 0; j < s.size(); j++)
+        freq[s[right]]++;
+
+        while(freq.size() > n)
         {
-            
+            freq[s[left]]--;
+            if(freq[s[left]] == 0) freq.erase(s[left]);
+            left++;
         }
+
+        res += (right - left + 1);
     }
+    return res;
+}
+
+int count_substring(string s, int n)
+{
+    //Exactly k = atMost(s, k) - atMost(s, k-1)
+    return op_ca(s, n) - op_ca(s, n - 1);
 }
 
 int main()
 {
     string s;
+    int n;
+    cin>>n;
+    cin.ignore();
     getline(cin, s);
-    int ans = op_ca(s);
+    int ans = count_substring(s, n);
     cout<<ans<<endl;
 }

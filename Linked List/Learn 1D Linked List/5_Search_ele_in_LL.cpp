8534c0ee1ad7b09c3c96f0981b48df2280d13f17
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-01-20 17:07:24 +0530

    DSA Practice

---
+++ Linked List/Learn 1D Linked List/5_Search_ele_in_LL.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int d, Node* n)
+    {
+        data = d;
+        next = n;
+    }
+    Node(int d)
+    {
+        data = d;
+        next = nullptr;
+    }
+};
+
+bool search_ele(Node* head, int el)
+{
+    Node* temp = head;
+    while(temp)
+    {
+        if(temp -> data == el) return true;
+    }
+    return false;
+}
+
+int main()
+{
+    Node* head = new Node(1);
+    head -> next = new Node(2);
+    head -> next -> next = new Node(3);
+
+    bool ans = search_ele(head, 2);
+    cout<<ans<<endl;
+}

ea93828eb01df76d43e5f1792ea43d626c1af6ae
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-01-31 14:34:14 +0530

    DSA Practice

---
+++ Bit Manipulation/Learn Bit Manipulation/3_Check_if_a_number_is_odd_or_not.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 12;
+    if((n & 1) == 0)cout<<"Even\n";
+    else cout<<"Odd\n";
+}
---
+++ Bit Manipulation/Learn Bit Manipulation/6_Set_Unset_the_rightmost_unset_bit.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 10;
+
+    cout<< (n | (n + 1))<<endl;
+}

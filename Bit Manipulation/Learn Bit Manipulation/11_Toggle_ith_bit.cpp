b1a73b352e924ba2550c1f58f3883520ee27163f
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-01-31 14:10:15 +0530

    DSA Practice

---
+++ Bit Manipulation/Learn Bit Manipulation/11_Toggle_ith_bit.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 13, i = 2;
+    cout<<(n ^ (1 << i))<<endl;
+}
---
+++ Bit Manipulation/Learn Bit Manipulation/9_Set_ith_bit_to_1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 9, i = 2;
+    cout<<((1 << i) | n)<<endl;
+}

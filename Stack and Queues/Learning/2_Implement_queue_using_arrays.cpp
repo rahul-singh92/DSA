9ae907f9f4259e82a36b1b516b1a091c89fa78b9
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-02-07 18:44:22 +0530

    DSA Practice

--- Stack and Queues/Learning/2_Implement_queue_using_arrays.cpp
+++ Stack and Queues/Learning/2_Implement_queue_using_arrays.cpp
@@ -1,38 +1,88 @@
 #include <iostream>
 using namespace std;
 
-class q_imp {
-    int qu[4];
-    int cu_size = 0;
-    int start = -1, end = -1;
+class ArrayQueue
+{
+    private:
+        int* arr;
+        int start, end;
+        int cur_size, max_size;
 
     public:
+        ArrayQueue()
+        {
+            arr = new int[10];
+            start = -1;
+            end = -1;
+            cur_size = 0;
+            max_size = 10;
+        }
+
+        ~ArrayQueue()
+        {
+            delete[] arr;
+        }
+
         void push(int x)
         {
-            if(cu_size == 4) cout<<"Cant push array is full.\n";
-            else if(cu_size == 0)
+            if(cur_size == max_size)
             {
-                start = 0, end = 0;
+                cout<<"Queue is full.\n";
+                exit(1);
+            }
+
+            if(end == -1)
+            {
+                start = 0;
+                end = 0;
             }
             else
             {
-                end = (end + 1) % 4;
+                end = (end + 1) % max_size;
             }
-            qu[end] = x;
-            cu_size++;
+
+            arr[end] = x;
+            cur_size++;
         }
 
-        void pop()
+        int pop()
         {
-            if(cu_size==0) cout<<"Empty";
-            if(cu_size == 1)
+            if(start == -1)
             {
-                start
+                cout<<"Queue is empty.\n";
+                exit(1);
             }
+            int popped = arr[start];
+
+            if(cur_size == 1)
+            {
+                start = -1;
+                end = -1;
+            }
+            else
+            {
+                start = (start + 1) % max_size;
+            }
+            cur_size--;
+            return popped;
+        }
+
+        int peek()
+        {
+            if(start == -1)
+            {
+                cout<<"Queue is empty.\n";
+                exit(1);
+            }
+            return arr[start];
         }
 };
 
 int main()
 {
-
+    ArrayQueue queue;
+    queue.push(10);
+    queue.push(5);
+    cout<<queue.pop()<<endl;
+    cout<<queue.peek()<<endl;
 }

0e9e7a1738f6e538fea9ec57baa202acd4f15c07
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-02-08 21:14:46 +0530

    DSA Practic

---
+++ Stack and Queues/Prefix_Infix_Postfix/5_Postfix_to_Infix.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void postfixToInfix(string s)
+{
+    stack<string> st;
+    string result;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(isalnum(s[i])) st.push(string(1, s[i]));
+
+        else {
+            string op2 = st.top(); st.pop();
+            string op1 = st.top(); st.pop();
+
+            st.push('(' + op1 + s[i] + op2 +')');
+        }
+    }
+    cout<<st.top()<<endl;
+}
+int main()
+{
+    string s = "AB-DE+F*/";
+    postfixToInfix(s);
+}

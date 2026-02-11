212840763b241ce85da0a52bd7866708c4aa9824
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-02-11 14:09:39 +0530

    DSA Practiced

---
+++ Sort/Sort_1/3_Insertion_Srotting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void insertion_sort(vector<int>& arr)
+{
+    int n = arr.size();
+
+    for(int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--; 
+        }
+
+        arr[j+1] = key;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {7, 4, 1, 5, 3}; 
+    insertion_sort(arr);
+
+    for(auto i: arr) cout<<i<<" ";
+    cout<<endl;
+}

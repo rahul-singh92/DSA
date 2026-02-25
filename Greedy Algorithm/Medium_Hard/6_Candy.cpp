d7d95aee5c4af99862adeab1be4977a657d77617
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-02-25 15:21:30 +0530

    DSA Practiv

--- Greedy Algorithm/Medium_Hard/6_Candy.cpp
+++ Greedy Algorithm/Medium_Hard/6_Candy.cpp
@@ -66,35 +66,39 @@ int bet_way(vector<int>& ratings)
 int op_way(vector<int>& ratings)
 {
     int n = ratings.size();
-    int candies = n;
 
-    int i = 1;
+    int sum = 1, i = 1;
     while(i < n)
     {
         if(ratings[i] == ratings[i-1])
         {
+            sum++;
             i++;
             continue;
         }
 
-        int peak = 0;
+        int peak = 1;
         while(i < n && ratings[i] > ratings[i-1])
         {
             peak++;
-            candies += peak;
+            sum+=peak;
             i++;
         }
 
-        int valley = 0;
+        int down = 1;
         while(i < n && ratings[i] < ratings[i-1])
         {
-            valley++;
-            candies += valley;
+            sum+= down;
             i++;
+            down++;
         }
-        candies -= min(peak, valley);
+
+        if(down > peak)
+        {
+            sum += (down - peak);
+        }
     }
-    return candies;
+    return sum;
 }
 
 int main()

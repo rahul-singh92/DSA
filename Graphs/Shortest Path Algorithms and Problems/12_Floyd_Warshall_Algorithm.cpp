6cd778c7d55450ce251b8f8fcf2243d7a5c02329
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-04-04 15:00:14 +0530

    DSA Prac

---
+++ Graphs/Shortest Path Algorithms and Problems/12_Floyd_Warshall_Algorithm.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void op_way(vector<vector<int>>& matrix)
+{
+    int n = matrix.size();
+    // for each intermediate node k
+    for(int k = 0; k < n; k++)
+    {
+        // check for every (i, j) pair
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(matrix[i][k] == -1 || matrix[k][j] == -1)
+                    continue;
+                
+                if(matrix[i][j] == -1)
+                {
+                    matrix[i][j] = matrix[i][k] + matrix[k][j];
+                }
+                else
+                {
+                    matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {
+        {0, 2, -1, -1},
+        {1, 0, 3, -1},
+        {-1, -1, 0, -1},
+        {3, 5, 4, 0}
+    };
+
+    op_way(matrix);
+    
+    for(int i = 0; i < matrix.size(); i++)
+    {
+        for(int j = 0; j < matrix.size(); j++)
+        {
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}

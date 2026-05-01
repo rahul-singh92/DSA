71d380108bf8fde0233f0f995ca7460f03de3418
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-05-01 16:21:31 +0530

    DSA Praac

---
+++ Graphs/Other Algorithms/3_Kosaraju's_Algorithm.cpp
@@ -0,0 +1,76 @@
+// Strongly connected component
+
+#include <iostream>
+using namespace std;
+
+void dfs(int node, vector<int>& vis, vector<vector<int>>& adj, stack<int>& st)
+{
+    vis[node] = 1;
+    for(auto it: adj[node])
+    {
+        if(!vis[it])
+        {
+            dfs(it, vis, adj, st);
+        }
+    }
+    st.push(node);
+}
+
+void dfs3(int node, vector<int>& vis, vector<vector<int>>& adjT)
+{
+    vis[node] = 1;
+    for(auto it: adjT[node]){
+        if(!vis[it])
+            dfs3(it, vis, adjT);
+    }
+}
+
+int kosaraju(int V, vector<vector<int>>& adj)
+{
+    vector<int> vis(V, 0);
+    stack<int> st;
+
+    for(int i = 0; i < V; i++)
+    {
+        if(!vis[i])
+            dfs(i, vis, adj, st);
+    }
+
+    vector<vector<int>> adjT(V);
+    for(int i = 0; i < V; i++)
+    {
+        vis[i] = 0;
+        for(auto it: adj[i])
+            adjT[it].push_back(i);
+    }
+
+    int scc = 0;
+    while(!st.empty())
+    {
+        int node = st.top();
+        st.pop();
+        if(!vis[node])
+        {
+            scc++;
+            dfs3(node, vis, adjT);
+        }
+    }
+    return scc;
+}
+
+int main()
+{
+    int n = 5;
+    vector<vector<int>> edges = {
+        {1, 0}, {0, 2}, {2, 1}, {0, 3}, {3, 4}
+    };
+
+    vector<vector<int>> adj(n);
+    for(int i = 0; i < 5; i++)
+    {
+        adj[edges[i][0]].push_back(edges[i][1]);
+    }
+
+    int ans = kosaraju(n, adj);
+    cout<<ans<<endl;
+}

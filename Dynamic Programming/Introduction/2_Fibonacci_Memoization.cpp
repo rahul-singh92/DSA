f6dbc361362c3ec8c0fb32bcd3d14aad314463eb
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-07-23 15:50:01 +0530

    Prac

--- Dynamic Programming/Introduction/2_Fibonacci_Memoization.cpp
+++ Dynamic Programming/Introduction/2_Fibonacci_Memoization.cpp
@@ -5,19 +5,19 @@
 #include <iostream>
 using namespace std;
 
-int fib(int n)
+int fib(int n, vector<int>& dp)
 {
-    vector<int> dp(n+1, -1);
     if(n <= 1) return n;
-
+    
     if(dp[n] != -1) return dp[n];
-
-    return dp[n] = fib(n-1) + fib(n-2);
+    
+    return dp[n] = fib(n-1, dp) + fib(n-2, dp);
 }
 
 int main()
 {
     int n = 5;
-    int a = fib(n);
+    vector<int> dp(n+1, -1);
+    int a = fib(n,dp);
     cout<<a<<endl;
 }
